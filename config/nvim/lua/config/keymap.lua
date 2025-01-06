5cdfef634e5a770f184c9655beecff31d3479ebf
TheDuggy <gkollegger5@outlook.com>
2025-01-06 21:12:34 +0100

    Working on harpoon-neovim key-mapping to close all harpooned files.

--- config/nvim/lua/config/keymap.lua
+++ config/nvim/lua/config/keymap.lua
@@ -3,5 +3,15 @@ local map = vim.keymap.set
 -- Harpoon
 local harpoon_mark = require('harpoon.mark')
 map('n', '<leader>a', harpoon_mark.add_file)
-map('n', '<leader>r', function () harpoon_mark.rm_file(harpoon_mark.get_current_index()) end)
+map('n', '<leader>r', function ()
+    harpoon_mark.rm_file(harpoon_mark.get_current_index())
+    vim.cmd('redrawtabline')
+end)
 map('n', '<leader>f', require('harpoon.ui').toggle_quick_menu)
+map('n', '<leader>s', function ()
+    vim.ui.input({ prompt ='Close harpooned tabs?', completion = 'y|n'}, function (input)
+        if input == 'y' then
+            harpoon_mark.clear_all()
+        end
+    end)
+end)
