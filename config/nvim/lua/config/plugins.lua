b64a8080836a7bc5d539a02addb9b00229247e32
the-real-nox <gkollegger5@outlook.com>
2025-01-07 09:50:57 +0100

    Started working on autocompletion on neovim.

--- config/nvim/lua/config/plugins.lua
+++ config/nvim/lua/config/plugins.lua
@@ -11,7 +11,18 @@ local lazy = {
     'nvim-treesitter/nvim-treesitter',
     'savq/melange-nvim',
     'ThePrimeagen/harpoon',
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
+
+    -- LSP
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    -- Snipppets
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip'
 }
 
 vim.list_extend(lazy, rocks)
