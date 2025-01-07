0facba512baf96bc1cc07daaaae787756b9c999e
the-real-nox <gkollegger5@outlook.com>
2025-01-07 08:48:27 +0100

    Fixed harpoon shortcuts. Added remaps for window-switching.

--- config/nvim/lua/config/keymap.lua
+++ config/nvim/lua/config/keymap.lua
@@ -2,16 +2,28 @@ local map = vim.keymap.set
 
 -- Harpoon
 local harpoon_mark = require('harpoon.mark')
-map('n', '<leader>a', harpoon_mark.add_file)
+
+function initTabbar()
+    vim.cmd('redrawtabline')
+    vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=NONE')
+    vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
+    vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+end
+
+map('n', '<leader>a', function ()
+    harpoon_mark.add_file()
+    initTabbar()
+end)
 map('n', '<leader>r', function ()
     harpoon_mark.rm_file(harpoon_mark.get_current_index())
-    vim.cmd('redrawtabline')
+    initTabbar()
 end)
 map('n', '<leader>f', require('harpoon.ui').toggle_quick_menu)
 map('n', '<leader>s', function ()
-    vim.ui.input({ prompt ='Close harpooned tabs?', completion = 'y|n'}, function (input)
+    vim.ui.input({ prompt ='Close harpooned tabs? ' }, function (input)
         if input == 'y' then
             harpoon_mark.clear_all()
+            initTabbar()
         end
     end)
 end)
--- config/nvim/lua/config/remap.lua
+++ config/nvim/lua/config/remap.lua
@@ -2,3 +2,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 local map = vim.keymap.set
+
+-- Window-switching
+map('n', '<leader>h', '<C-w>h')
+map('n', '<leader>j', '<C-w>j')
+map('n', '<leader>k', '<C-w>k')
+map('n', '<leader>l', '<C-w>l')
