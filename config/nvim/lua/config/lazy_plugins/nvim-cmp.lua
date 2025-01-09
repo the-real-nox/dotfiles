f20719fa9659c3c717260ddee5afdb2a2e1473d0
the-real-nox <gkollegger5@outlook.com>
2025-01-09 12:49:40 +0100

    Added lsp-diagnostics in insert mode into neovim.

--- config/nvim/lua/config/lazy_plugins/nvim-cmp.lua
+++ config/nvim/lua/config/lazy_plugins/nvim-cmp.lua
@@ -19,8 +19,8 @@
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['q'] = cmp.mapping.abort(),
+      ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
@@ -31,7 +31,6 @@
       { name = 'buffer' },
     })
   })
-
   -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
   -- Set configuration for specific filetype.
   --[[ cmp.setup.filetype('gitcommit', {
@@ -41,7 +40,7 @@
       { name = 'buffer' },
     })
  })
- require("cmp_git").setup() ]]-- 
+ require("cmp_git").setup() ]]--
 
   -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline({ '/', '?' }, {
@@ -62,3 +61,6 @@
     matching = { disallow_symbol_nonprefix_matching = false }
   })
 
+vim.diagnostic.config({
+    update_in_insert = true
+})
