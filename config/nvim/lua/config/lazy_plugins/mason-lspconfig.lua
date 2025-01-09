9c27cc1e2d705c858c64c754e039d6cf055f7c05
the-real-nox <gkollegger5@outlook.com>
2025-01-09 10:01:26 +0100

    Added remap for searching. lspconfig with mason doesn't work yet.

--- config/nvim/lua/config/lazy_plugins/mason-lspconfig.lua
+++ config/nvim/lua/config/lazy_plugins/mason-lspconfig.lua
@@ -1,7 +1,16 @@
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local handlers = {
+    function (name)
+        require('lspconfig')[name].setup({capabilities = capabilities})
+    end
+}
+
 local ensure_installed = {
     "lua_ls"
-} 
+}
 
 require("mason-lspconfig").setup {
     ensure_installed = ensure_installed,
+    handlers = handlers,
 }
