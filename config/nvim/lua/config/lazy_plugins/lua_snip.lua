d52bcb84a8c682eea7fd07a38f4c6fd90312112f
the-real-nox <the-real-nox@outlook.com>
2025-10-17 18:46:46 +0200

    Removed debug-prints.

--- config/nvim/lua/config/lazy_plugins/lua_snip.lua
+++ config/nvim/lua/config/lazy_plugins/lua_snip.lua
@@ -1,5 +1,4 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
-vim.notify('snippet!')
 
 local ls = require('luasnip')
