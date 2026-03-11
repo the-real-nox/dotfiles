6e66a5897da477bdc54cabb74df98e6e39ff05f4
noxie64 <the-real-nox@outlook.com>
2026-03-11 12:04:08 +0100

    Updated wired_log script.

--- config/wired/wired_log.sh
+++ config/wired/wired_log.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 NOTIFICATION_LOG_FILE="$HOME/Notifications/wired.log"
 
-jq -r '"\u001b[32m[\u001b[33m\(.time | strftime("%H:%M:%S"))\u001b[32m] \u001b[36m\(.summary): \u001b[0m\(.body)"' \
+jq -r '
+"\u001b[32m" as $green  |
+"\u001b[33m" as $yellow |
+"\u001b[0m"  as $reset  |
+"\u001b[39m" as $white  |
+"\u001b[36m" as $cyan   |
+if .urgency == "Critical" then
+    "\u001b[41m\u001b[37m"
+elif .urgency == "Normal" then
+    "\($yellow)"
+else
+    "\($white)"
+end as $urgency_color |
+if .urgency == "Critical" then
+    "\($urgency_color)[-]"
+elif .urgency == "Normal" then
+    "\($urgency_color)[!]"
+else
+    "\($urgency_color)[+]"
+end as $urgency |
+"\($green)[\($cyan)\(.time | strftime("%H:%M:%S"))\($green)] - \($urgency)\($reset)\($green) - \($cyan)\(.summary): \($urgency_color)\(.body)\($reset)"' \
     "$NOTIFICATION_LOG_FILE" | less
